540d644c9320c41f23e7e40170bef17154c0288a
Bastián <bastian.sepulveda2001@alumnos.ubiobio.cl>
2024-05-05 22:13:47 -0400

    Commit 2/ Angular 4: Directivas parte 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { allIcons } from 'ngx-bootstrap-icons';
+import { IProduct } from './product';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,26 @@ import { allIcons } from 'ngx-bootstrap-icons';
 })
 export class AppComponent {
   title = 'Empresa ACME';
+  products: IProduct[] = [
+    {
+      productId: 1,
+      productName: 'Celular',
+      productCode: 'CEL-0011',
+      releaseDate: 'March 19, 2021',
+      price: 1000,
+      description: 'Celular de última geração',
+      starRating: 4.2,
+      imageUrl: 'https://cdn.pixabay.com/photo/2017/04/03/15/52/mobile-phone-2198770_1280.png'
+    },
+    {
+      productId: 2,
+      productName: 'Notebook',
+      productCode: 'NTB-0011',
+      releaseDate: 'March 18, 2021',
+      price: 2000,
+      description: 'Notebook de última geração',
+      starRating: 4.6,
+      imageUrl: 'https://cdn.pixabay.com/photo/2016/06/28/05/10/laptop-1483974_1280.jpg'
+    }
+  ];
 }
